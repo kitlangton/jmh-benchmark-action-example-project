43cef4c19e391d332c44a4a7374232a2ce736865
Kit Langton <kit.langton@gmail.com>
2023-08-04 11:56:24 -0400

    change things around

--- benchmarks/src/main/scala/benchmarks/Benchmarks.scala
+++ benchmarks/src/main/scala/benchmarks/Benchmarks.scala
@@ -8,13 +8,13 @@ class MyBenchmark {
   @Benchmark
   def measure(): Unit = {
     val x    = 1 + 1
-    val slow = List.fill(7_000)(x).reverse
+    val slow = List.fill(5_000)(x).reverse
   }
 
   @Benchmark
   def measure2(): Unit = {
     val x    = 1 + 2
-    val slow = List.fill(53_100)(x).reverse
+    val slow = List.fill(83_100)(x).reverse
   }
 
 }
